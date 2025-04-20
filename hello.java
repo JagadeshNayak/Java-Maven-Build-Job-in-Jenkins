a78ec36c1981042cc8366e7f49ffcc2852245de5
Jagadesh Nayak <137325962+JagadeshNayak@users.noreply.github.com>
2025-04-20 15:10:11 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,2 @@
-// HelloWorld.java
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins + Maven!");
-    }
-}
+// HelloWorld.java public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, Jenkins +
+Maven!"); } }
